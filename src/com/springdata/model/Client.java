113d0059360942cca8d465c6a10995839b90a172
Hassan Askar <hassanemad15697@gmail.com>
2022-01-05 15:49:47 +0100

    creating client model

---
+++ src/com/springdata/model/Client.java
@@ -0,0 +1,35 @@
+package com.springdata.model;
+
+public class Client {
+
+	private long id;
+	private String name;
+	private String address;
+	private int age;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	
+}
