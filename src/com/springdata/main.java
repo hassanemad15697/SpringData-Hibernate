7f6aba5d279b30938b7b76e391e76e3358e6d690
Hassan Askar <hassanemad15697@gmail.com>
2022-01-05 16:27:36 +0100

    changing hibernate jars & testing with sessionFactory , Session

--- src/com/springdata/main.java
+++ src/com/springdata/main.java
@@ -4,16 +4,30 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.springdata.model.Client;
+
 public class main {
 
 	public static void main(String[] args) throws SQLException {
-		final String host="jdbc:mysql://localhost/employee";
+		
+		SessionFactory factory = new Configuration().configure("hibernate.cfg.xml")
+													.addAnnotatedClass(Client.class)
+													.buildSessionFactory();
+		
+		Session session=factory.getCurrentSession();
+		
+		
+		/*final String host="jdbc:mysql://localhost/employee";
 		final String username = "DBHassan";
 		final String password="ELKBEER36647126358#";
 		Connection conn;
 		conn = DriverManager.getConnection(host, username, password);
         System.out.println("Connected!");
-        conn.close();
+        conn.close();*/
 	}
 
 }
